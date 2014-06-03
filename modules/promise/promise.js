dad320e1e24d6d7ca4ee91706387163236ebc72a
Feifei HANG <l.daniel.hung@gmail.com>
2014-06-03 16:28:57 +0100

    chop.js promise module

--- modules/promise/promise.js
+++ modules/promise/promise.js
@@ -2,29 +2,28 @@
 $ch.define('promise', function () {
   'use strict';
 
-  var promise = {
-    promise: this,
-    data: undefined,
-    callbacks: [],
-
-    then: function (callback) {
-      this.callbacks.push(callback);
-      return this;
-    },
-
-    resolve: function (data) {
-      this.data = data;
-      console.log('Check: ' + this.data);
-      $$CHOP.each(this.callbacks, function (callback) {
-        this.data = callback(this.data);
-      });
-    }
-  };
-
   $$CHOP.promise = {};
   $$CHOP.promise = {
     defer: function () {
-      return Object.create(promise);
+      var promise = {
+        promise: this,
+        data: undefined,
+        callbacks: [],
+
+        then: function (callback) {
+          this.callbacks.push(callback);
+          return this;
+        },
+
+        resolve: function (data) {
+          this.data = data;
+          var that = this;
+          $$CHOP.each(this.callbacks, function (callback) {
+            that.data = callback(that.data);
+          });
+        }
+      };
+      return promise;
     }
   };
 
