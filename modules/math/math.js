64db34cbcc8b298eda1487f19d8a3eb535e68dc0
Feifei HANG <l.daniel.hung@gmail.com>
2014-06-04 10:39:10 +0100

    better algorithm

--- modules/math/math.js
+++ modules/math/math.js
@@ -14,7 +14,26 @@ $ch.define('math', function () {
       }
 
       var value = eval(calc);
-      return Math.round(value * 100) / 100;
+      var valueStr = value + '';
+      var factor = 0;
+      var prev = '', startMatch = false;
+      for (var index = valueStr.length - 1; index !== -1; --index) {
+        var digit = valueStr[index];
+        if (digit === prev) {
+          if (startMatch === false) {
+            startMatch = true;
+          }
+          ++factor;
+        } else {
+          if (startMatch === true) {
+            break;
+          }
+        }
+        prev = digit;
+      }
+      factor = Math.pow(10, factor);
+
+      return Math.round(value * factor) / factor;
     }
   };
 });
