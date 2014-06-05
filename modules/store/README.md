907a843b65885dcc08c05b072c303436b6580384
Feifei HANG <l.daniel.hung@gmail.com>
2014-06-05 17:01:24 +0100

    cookies

--- modules/store/README.md
+++ modules/store/README.md
@@ -6,8 +6,20 @@ Dependences
 
 None
 
-$ch.store.local(`key`, `data`)
+$ch.store.local(`key`[, `data`])
 ------------------------------
 
 If only `key` provided, returns the value of `key` from local storage.
 Otherwise, sets and saves `key` to `data`.
+
+$ch.store.cookies(`key`[, `value`])
+-----------------------------------
+
+If only `key` provides:
+
+- when `key` is an object: sets cookies to `key`.
+- otherwise, returns the data of `key` stored in cookies.
+
+Otherwise, sets value of `key` to `value` and saves in cookies.
+
+
--- modules/store/store.js
+++ modules/store/store.js
@@ -26,6 +26,51 @@ $ch.define('store', function () {
 
         localStorage.setItem(key, JSON.stringify(value));
       }
+    },
+
+    cookies: function (key, value) {
+      if (document.cookies === undefined) {
+        throw new Error('Cookies is not supported by this browser.');
+      }
+
+      var obj;
+      if (arguments.length === 0) {
+        obj = document.cookies;
+        if (obj === undefined) {
+          return {};
+        }
+        return JSON.parse(obj);
+      }
+
+      if (arguments.length === 1) {
+        if (typeof key === 'object') {
+          var str = JSON.stringify(key);
+          document.cookies = str;
+        }
+
+        if (typeof key === 'string') {
+          obj = document.cookies;
+          if (obj === undefined) {
+            obj = {};
+          } else {
+            obj = JSON.parse(obj);
+          }
+          return obj[key];
+        }
+      }
+
+      if (arguments.length === 2) {
+        obj = document.cookies;
+        if (obj === undefined) {
+          obj = {};
+        } else {
+          obj = JSON.parse(obj);
+        }
+
+        obj[key] = {};
+        obj[key] = value;
+        document.cookies(JSON.parse(obj));
+      }
     }
 
   };
