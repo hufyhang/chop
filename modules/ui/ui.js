7dcf288efb3f18e6b5f7d12b8d47c50b2fb8e9bc
Feifei HANG <l.daniel.hung@gmail.com>
2014-06-09 13:58:45 +0100

    ui style

--- modules/ui/ui.js
+++ modules/ui/ui.js
@@ -1,12 +1,23 @@
 /* global $ch, $$CHOP, $$CHOPEL */
 $ch.define('ui', function () {
   'use strict';
-  $$CHOP.find('html').css('fontFamily', 'Arial, sans-serif');
-  var hrs = $$CHOP.findAll('hr');
-  $$CHOP.each(hrs, function (hr) {
-    hr.css('height', '1px').css('border', '0')
-    .css('border-top', '1px solid #ccc').css('margin', '1em').css('padding', '0');
-  });
+  var UI_CSS = 'http://feifeihang.info/chop/style.php?q=chopjs-ui-style';
+
+  var node = document.createElement('style');
+  node.innerHTML = $$CHOP.http({url: UI_CSS, async: false});
+  document.head.appendChild(node);
+
+  // hide and show//{{{
+  $$CHOPEL.hide = function () {
+    this.el.className += 'chopjs-ui-hide';
+    return this;
+  };
+
+  $$CHOPEL.show = function () {
+    this.el.className = this.el.className.replace(/chopjs-ui-hide/g, '');
+    return this;
+  };
+//}}}
 
   // height//{{{
   $$CHOPEL.height = function () {
@@ -31,20 +42,19 @@ $ch.define('ui', function () {
   // full size//{{{
   $$CHOPEL.full = function () {
     var e = this.el;
-    e.style.width = '100%';
-    e.style.height = '100%';
+    e.className += 'chopjs-ui-full';
     return this;
   };
 
   $$CHOPEL.fullHeight = function () {
     var e = this.el;
-    e.style.height = '100%';
+    e.className += 'chopjs-ui-full-height';
     return this;
   };
 
   $$CHOPEL.fullWidth = function () {
     var e = this.el;
-    e.style.width = '100%';
+    e.className += 'chopjs-ui-full-width';
     return this;
   };
 //}}}
@@ -52,52 +62,24 @@ $ch.define('ui', function () {
   // disable & enable//{{{
   $$CHOPEL.disable = function () {
     this.attr('disabled', 'disabled');
-    this.css('cursor', 'not-allowed');
+    this.el.className += 'chopjs-ui-disable';
     return this;
   };
 
   $$CHOPEL.enable = function () {
     this.removeAttr('disabled');
-    this.css('cursor', 'initial');
+    this.el.className = this.el.className.replace(/chopjs-ui-disable/g, '');
     return this;
   };
 //}}}
 
   // click button//{{{
   $$CHOPEL.button = function (callback) {
-    var element = this.el;
-
-    element.style.borderColor = 'rgba(0, 0, 0, 0.6)';
-    element.style.textShadow = '1px 1px 1px rgba(0, 0, 0, 0.3)';
-    element.style.color = '#ffffff';
-    element.style.background = '#4391e3';
-    element.style.display = 'inline-block';
-    element.style.fontWeight = 'normal';
-    element.style.textAlign = 'center';
-    element.style.verticalAlign = 'middle';
-    element.style.whiteSpace = 'no-wrap';
-    element.style.cursor = 'pointer';
-    element.style.padding = '8px 12px';
-    element.style.fontSize = '14px';
-    element.style.borderRadius = '4px';
-    element.style.border = '1px solid transparent';
-    element.style.userSelect = 'none';
-    element.style.webkitUserSelect = 'none';
-    element.style.mozUserSelect = 'none';
-    element.style.msUserSelect = 'none';
-    element.style.fontFamily = 'Arial, sans-serif';
+    this.el.className += 'chopjs-ui-button';
 
     if (callback !== undefined) {
       this.on('click', callback);
     }
-
-    this.on('mouseover', function () {
-      element.style.background = '#539DEB';
-    });
-
-    this.on('mouseout', function () {
-      element.style.background = '#4391e3';
-    });
 
     return this;
   };
@@ -105,29 +87,12 @@ $ch.define('ui', function () {
 
   // input & textarea //{{{
   $$CHOPEL.input = function (callback) {
-    var e = this.el;
-
-    e.style.display = 'inline-block';
-    e.style.padding = '8px 12px';
-    e.style.fontSize = '14px';
-    e.style.color = '#555555';
-    e.style.backgroundColor = '#ffffff';
-    e.style.backgroundImage = 'none';
-    e.style.border = '1px solid #cccccc';
-    e.style.borderRadius = '4px';
-    e.style.fontFamily = 'Arial, sans-serif';
+    this.el.className += 'chopjs-ui-input';
 
     if (callback !== undefined) {
       this.on('keyup', callback);
     }
 
-    this.on('focus', function () {
-      e.style.borderColor = '#66afe9';
-      e.style.outline = 0;
-    }).on('blur', function () {
-      e.style.borderColor = '#cccccc';
-      e.style.outline = 0;
-    });
     return this;
   };
 
@@ -137,28 +102,13 @@ $ch.define('ui', function () {
   // dropbox & selectbox //{{{
   $$CHOPEL.dropbox = function (callback) {
     var e = this.el;
-
-    e.style.padding = '8px 12px';
-    e.style.fontSize = '14px';
-    e.style.color = '#555555';
-    e.style.backgroundColor = '#ffffff';
-    e.style.backgroundImage = 'none';
-    e.style.border = '1px solid #cccccc';
-    e.style.borderRadius = '4px';
+    this.el.className += 'chopjs-ui-select';
 
     if (callback !== undefined) {
       this.on('change', function () {
         callback(e.value);
       });
     }
-
-    this.on('focus', function () {
-      e.style.borderColor = '#66afe9';
-      e.style.outline = 0;
-    }).on('blur', function () {
-      e.style.borderColor = '#cccccc';
-      e.style.outline = 0;
-    });
 
     return this;
   };
@@ -168,47 +118,11 @@ $ch.define('ui', function () {
 
   // pager button//{{{
   $$CHOPEL.pager = function (callback) {
-    var element = this.el;
-
-    element.style.color = '#2fa4e7';
-    element.style.textShadow = 'none';
-    element.style.background = '#ffffff';
-    element.style.display = 'inline-block';
-    element.style.fontWeight = 'normal';
-    element.style.textAlign = 'center';
-    element.style.verticalAlign = 'middle';
-    element.style.whiteSpace = 'no-wrap';
-    element.style.cursor = 'pointer';
-    element.style.padding = '8px 20px';
-    element.style.fontSize = '14px';
-    element.style.borderRadius = '15px';
-    element.style.border = '1px solid #dddddd';
-    element.style.userSelect = 'none';
-    element.style.webkitUserSelect = 'none';
-    element.style.mozUserSelect = 'none';
-    element.style.msUserSelect = 'none';
-    element.style.fontFamily = 'Arial, sans-serif';
-    element.style.lineHeight = '1em';
+    this.el.className += 'chopjs-ui-pager';
 
     if (callback !== undefined) {
       this.on('click', callback);
     }
-
-    this.on('mouseover', function () {
-      element.style.background = '#dddddd';
-      element.style.color = '#157ab5';
-    }).on('mouseout', function () {
-      element.style.background = '#ffffff';
-      element.style.color = '#2fa4e7';
-    });
-
-    this.on('focus', function () {
-      element.style.borderColor = '#66afe9';
-      element.style.outline = 0;
-    }).on('blur', function () {
-      element.style.borderColor = '#cccccc';
-      element.style.outline = 0;
-    });
 
     return this;
   };
@@ -222,18 +136,9 @@ $ch.define('ui', function () {
       zindex = 1;
     }
 
-    e.style.position = 'fixed';
-    e.style.display = 'inline-block';
+    e.className += 'chopjs-ui-topbar';
     e.style.zindex = zindex;
-    e.style.top = '0';
-    e.style.left = '0';
-    e.style.right = '0';
-    e.style.background = 'rgb(67, 145, 227)';
-    e.style.color = 'white';
-    e.style.border = '1px solid transparent';
-    e.style.paddingTop = '5px';
-    e.style.paddingBottom = '5px';
-    e.style.boxShadow = '0px 1px 1px #3b5998';
+
 
     var subs = $ch.findAll('*', e);
     $ch.each(subs, function (sub) {
@@ -242,12 +147,10 @@ $ch.define('ui', function () {
         if (sub.el.getAttribute('ch-logo') === null) {
           sub.button();
         } else {
-          $ch.find('img', sub.el).css('max-height', '30px');
+          sub.el.className += '.ch-topbar-img';
         }
       }
-      sub.el.style.fontSize = '20px';
-      sub.el.style.display = 'table-cell';
-      sub.el.style.padding = '2px 3px';
+      sub.el.className += 'ch-topbar-element';
     });
 
     if (next !== undefined) {
---
+++ php/style.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: text/css;charset=utf-8');
+
+$GITHUB = 'https://raw.githubusercontent.com/hufyhang/chop/master/css/';
+$module = $_GET['module'];
+$url = $GITHUB . $module . '.css';
+echo url_get_contents($url);
+
+function url_get_contents ($url) {
+    if (!function_exists('curl_init')){
+        die('CURL is not installed!');
+    }
+    $ch = curl_init();
+    curl_setopt_array( $ch, array(
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_URL => $url) );
+    curl_setopt($ch, CURLOPT_VERBOSE, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+?>
