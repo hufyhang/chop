71a031c91292d311056f3360a09a08de72deacf5
Feifei HANG <l.daniel.hung@gmail.com>
2014-09-28 14:59:27 +0100

    gangnam-style

---
+++ directives/gangnam-style/gangnam-style/main.js
@@ -0,0 +1,26 @@
+/* global $ch */
+$ch.define('gangnam-style/main', function () {
+  'use strict';
+
+  $ch.require('directive');
+
+  var template = $ch.http({
+    url: 'gangnam-style/template.html',
+    async: false
+  }).responseText;
+
+  $ch.directive.add('gangnam-style', template, function (com, shadow) {
+    var audio = new Audio();
+    audio.src= 'gangnam-style/gangnam.ogg';
+    audio.autoplay = false;
+    audio.loop = true;
+    audio.load();
+
+    $ch.find('#sig', shadow).on('mouseover', function () {
+      audio.play();
+    }).on('mouseout', function () {
+      audio.pause();
+    });
+  });
+
+});
---
+++ directives/gangnam-style/main.js
@@ -0,0 +1,2 @@
+/* global $ch */
+$ch.require('gangnam-style/main');
