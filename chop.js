7cac34aedd0950c8d983ec524bdbc17e76bc0f84
Feifei HANG <l.daniel.hung@gmail.com>
2014-12-07 21:12:35 +0000

    router module hosted on CDN

--- chop.js
+++ chop.js
@@ -39,7 +39,7 @@
     'promise'   : '0.1',
     'rdfa'      : '0.1',
     'regex'     : '0.1',
-    'router'    : '0.3', // Updated
+    'router'    : '0.3',
     'scope'     : '0.2',
     'state'     : '0.1',
     'store'     : '0.1',
